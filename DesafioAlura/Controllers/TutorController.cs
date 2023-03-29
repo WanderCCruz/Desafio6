af5e943f569aaf95dd8cf8b91a29f45c92cf219f
Wander Cassio da Cruz Almeida <wander.ccruz@gmail.com>
2023-03-28 23:49:50 -0300

    inicio da criacao crud tutor

---
+++ DesafioAlura/Controllers/TutorController.cs
@@ -0,0 +1,61 @@
+﻿using DesafioAlura.Context;
+using DesafioAlura.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioAlura.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class TutorController : ControllerBase
+    {
+        private AdoPetContext _context;
+
+        public TutorController(AdoPetContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult AdicionaTutor([FromBody] Tutor tutor)
+        {
+            _context.Tutores.Add(tutor);
+            _context.SaveChanges();
+            return StatusCode(201);
+        }
+
+        [HttpGet]
+        public IEnumerable<Tutor> RecuperarTutores()
+        {
+            return _context.Tutores;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaTutorPorId(int id)
+        {
+            var tutor = _context.Tutores.FirstOrDefault(t => t.Id == id);
+            if (tutor == null) return NotFound();
+            return Ok(tutor);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizaTutor(int id, [FromBody] Tutor tutorUpdate)
+        {
+            var tutor = _context.Tutores.FirstOrDefault(t => t.Id == id);
+            if (tutor == null) return NotFound();
+            tutor = tutorUpdate;
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTutor(int id)
+        {
+            var tutor = _context.Tutores.FirstOrDefault(t => t.Id == id);
+            if (tutor == null) return NotFound();
+            _context.Remove(tutor);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
--- DesafioAlura/Program.cs
+++ DesafioAlura/Program.cs
@@ -10,16 +10,15 @@ builder.Services.AddDbContext<AdoPetContext>(options => options.UseSqlServer(con
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
