58a2fea39a7ae7c3504d33e6e8d4482a15a37753
Wander Cassio da Cruz Almeida <wander.ccruz@gmail.com>
2023-04-24 23:56:59 -0300

    Alterações de validação

--- AdoPet.Testes/UsuarioTeste.cs
+++ AdoPet.Testes/UsuarioTeste.cs
@@ -1,6 +1,6 @@
 namespace AdoPet.Testes
 {
-    public class UnitTest1
+    public class UsuarioTeste
     {
         [Fact]
         public void Test1()
--- DesafioAlura/Validadores/CreateUsuarioValidator.cs
+++ DesafioAlura/Validadores/CreateUsuarioValidator.cs
@@ -11,8 +11,11 @@ namespace DesafioAlura.Validadores
             RuleFor(nome => nome.Nome).NotEmpty().NotNull().WithMessage("Campo obrigatório");
             RuleFor(nome => nome.Nome).MaximumLength(150).MinimumLength(10).WithMessage("O campo deve ter entre 10 e 150 caracteres");
             RuleFor(senha => senha.Senha).NotEmpty().NotNull().WithMessage("Campo obrigatório");
+            RuleFor(senha => senha.Senha).Matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z$*&@#]{8,}$").
+                WithMessage("A senha deve ter letras maiúsculas, minúsculas, numeros e conter pelo menos 8 digitos");
             RuleFor(senha => senha.Senha).MaximumLength(50).MinimumLength(6).WithMessage("O campo deve ter entre 6 e 50 caracteres");
-            RuleFor(email => email.Email).NotEmpty().NotNull().WithMessage("Campo obrigatório");
+            RuleFor(email => email.Email).NotEmpty().NotNull().WithMessage("Campo obrigatório").EmailAddress();
+            RuleFor(email => email.Email).EmailAddress().WithMessage("Email incorreto");
             RuleFor(email => email.Email).MaximumLength(150).MinimumLength(10).WithMessage("O campo deve ter entre 6 e 50 caracteres");
             RuleFor(sobre => sobre.Sobre).NotEmpty().NotNull().WithMessage("Campo obrigatório");
             RuleFor(sobre => sobre.Sobre).MaximumLength(150).MinimumLength(10).WithMessage("O campo deve ter entre 6 e 50 caracteres");
